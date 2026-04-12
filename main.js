1804895998f647a2b548ecab9ab7dc0aa3567d72
Kvothe9 <rprobertopelayo@gmail.com>
2026-04-12 14:16:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -192,7 +192,7 @@ function mostrarPokemon(data) {
 
     <div class="card_extra">
         <div class="extra_content">
-            <h2 class="name_pokemon">${data.name}</h2>
+            <h4 class="name_pokemon">${data.name}</h4>
             <div class="stats_container">
                 ${stats}
             </div>
